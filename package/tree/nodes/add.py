eb42e4268db63017e180a6abea2eaad5cfa1bde4
jenuk <gandalf.d@web.de>
2020-03-12 11:24:08 +0100

    simplify addition

--- package/tree/nodes/add.py
+++ package/tree/nodes/add.py
@@ -1,4 +1,5 @@
 from ..node import Node, operator_dict
+from .number import NumberNode
 
 class AddNode(Node):
     symb = "+"
@@ -7,8 +8,41 @@ class AddNode(Node):
     acts_as_number = False
 
     def apply(self):
-        if self.first is None or self.last is None:
+        if self.first is None:
             raise MalformedExpressionException("Not enough arguments for addition")
-        return self.first.apply() + self.last.apply()
+        res = self.first.apply()
+        for child in self.children[1:]:
+            res = res + child.apply()
+        return res
+
+    def simplify(self):
+        new_children = []
+        for child in self.children:
+            child = child.simplify()
+            if type(child) == AddNode:
+                new_children.extend(child.children)
+            else:
+                new_children.append(child)
+
+        self.children = new_children
+        new_children = []
+
+        nn = None # numbernode
+        for child in self.children:
+            if type(child) == NumberNode:
+                if nn is None:
+                    nn = child
+                    new_children.append(child)
+                else:
+                    nn.value = nn.value + child.value
+            else:
+                new_children.append(child)
+
+        self.children = new_children
+
+        if len(self.children) == 1:
+            return self.first
+        else:
+            return self
 
 operator_dict["+"] = AddNode
